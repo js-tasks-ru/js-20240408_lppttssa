dd308ef7c9231dd9db2eb891bd1584cca4a21f9c
Anastasiia <a.lonshakova@brl.ru>
2024-04-10 21:08:37 +0700

    add array sorting

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,9 +1,20 @@
+const compareStrings = (firstString, secondString) => {
+  return firstString.localeCompare(secondString, 'ru', {caseFirst: "upper"});
+};
+
 /**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+export function sortStrings(arr, param = 'asc') {
+  const sortedArray = [...arr].sort(compareStrings);
+
+  if (param === 'desc') {
+    return sortedArray.reverse();
+  }
+
+  return sortedArray;
 }
