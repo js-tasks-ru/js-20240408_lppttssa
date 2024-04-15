48877e2c508459bcf040dc07cd53fc9c0dd5fad8
Anastasiia <a.lonshakova@brl.ru>
2024-04-15 22:50:46 +0700

    add getter [review]

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,18 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  return function getter(obj) {
+    if (Object.keys(obj).length === 0) {
+      return undefined;
+    }
 
+    const keys = path.split('.');
+
+    let currentObj = obj;
+    keys.forEach(key => {
+      currentObj = currentObj[key];
+    });
+
+    return currentObj;
+  };
 }
