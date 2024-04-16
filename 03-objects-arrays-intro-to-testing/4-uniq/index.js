f31550c8d9fbae0b3d54866eff20b420951f1823
Anastasiia <a.lonshakova@brl.ru>
2024-04-16 12:46:18 +0700

    add uniq function

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,15 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  const resultArray = [];
+  let filteredArray = arr ? [...arr] : [];
 
+  while (filteredArray.length > 0) {
+    resultArray.push(filteredArray[0]);
+
+    const elementToFilter = filteredArray[0];
+    filteredArray = filteredArray.filter(item => item !== elementToFilter);
+  }
+
+  return resultArray;
 }
