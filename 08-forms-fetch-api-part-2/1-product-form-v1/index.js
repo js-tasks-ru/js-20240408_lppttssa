dbcaad34c27421b2887be442dff603f4aed60086
Anastasiia <a.lonshakova@brl.ru>
2024-05-21 22:47:54 +0700

    add sortable list

--- 08-forms-fetch-api-part-2/1-product-form-v1/index.js
+++ 08-forms-fetch-api-part-2/1-product-form-v1/index.js
@@ -4,7 +4,7 @@ import fetchJson from './utils/fetch-json.js';
 const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class ProductForm {
+export default class ProductFormV1 {
   element = null;
   subElements;
 
--- 09-tests-for-frontend-apps/1-product-form-v2/index.js
+++ 09-tests-for-frontend-apps/1-product-form-v2/index.js
@@ -1,13 +1,14 @@
 import SortableList from '../../2-sortable-list/src/index.js';
 import escapeHtml from './utils/escape-html.js';
 import fetchJson from './utils/fetch-json.js';
+import ProductFormV1 from "../../08-forms-fetch-api-part-2/1-product-form-v1";
 
 const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class ProductForm {
+export default class ProductForm extends ProductFormV1 {
   constructor (productId) {
-    this.productId = productId;
+    super(productId)
   }
 
   async render () {
